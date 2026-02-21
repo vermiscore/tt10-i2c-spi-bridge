77bfb6d403878c7068f5e5c04523b8b67fbf40d4
vermiscore <vermiscore@users.noreply.github.com>
2026-02-20 23:44:45 -0800

    fix: update tb.v module name

--- test/tb.v
+++ test/tb.v
@@ -27,8 +27,8 @@ module tb ();
   wire VGND = 1'b0;
 `endif
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  // Replace tt_um_i2c_spi_bridge with your module name:
+  tt_um_i2c_spi_bridge user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
